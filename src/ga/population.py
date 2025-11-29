7a1f715efea75966d3cb90b049892c3c885aaf39
Yusef <youssefmohmed430@gmail.com>
2025-11-29 16:46:13 +0200

    Implement population generation for tournament schedules

--- src/ga/population.py
+++ src/ga/population.py
@@ -1,1 +1,10 @@
+from ga.individual import ScheduleIndividual
 
+class Population:
+
+    def generate_population(self, teams, population_size):
+        population = []
+        for _ in range(population_size):
+            individual = ScheduleIndividual(teams)
+            population.append(individual)
+        return population
--- src/main.py
+++ src/main.py
@@ -7,10 +7,20 @@ from ga.individual import ScheduleIndividual
 teams1 = ["A","B","C","D"]
 schedule1 = ScheduleIndividual(teams1)
 schedule1.display()
- 
 print("----------------------------------------------")
 
-#test for odd number of teams
-teams2 = ["A","B","C","D","E"]
-schedule2 = ScheduleIndividual(teams2)
+
+teams1 = ["A","B","C","D"]
+schedule3 = ScheduleIndividual(teams1)
+schedule3.display()
+print("----------------------------------------------")
+
+teams1 = ["A","B","C","D"]
+schedule2 = ScheduleIndividual(teams1)
 schedule2.display()
+ 
+
+#test for odd number of teams
+# teams2 = ["A","B","C","D","E"]
+# schedule2 = ScheduleIndividual(teams2)
+# schedule2.display()
