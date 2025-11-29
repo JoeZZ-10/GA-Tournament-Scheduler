70bf712907f19215f0d4fb3e6ab3012af07bdb26
JoeZZ <youssefislammoenes@gmail.com>
2025-11-29 13:44:51 +0200

    Added venues and timeslots

--- src/main.py
+++ src/main.py
@@ -3,14 +3,11 @@
 #import class
 from ga.individual import ScheduleIndividual
 
-#test for even number of teams
-teams1 = ["A","B","C","D"]
-schedule1 = ScheduleIndividual(teams1)
-schedule1.display()
- 
-print("----------------------------------------------")
 
-#test for odd number of teams
-teams2 = ["A","B","C","D","E"]
-schedule2 = ScheduleIndividual(teams2)
-schedule2.display()
+teams = ["A","B","C","D","E"]
+venues = ["V1","V2","V3"]
+timeslots = ["09:00", "13:00"]
+schedule = ScheduleIndividual(teams, venues, timeslots, randomize=True)
+schedule.display()
+ 
+
