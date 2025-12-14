80c90609c0c561fc6241bb4bdb3bf386304627b8
Mohamed <mohamed@example.com>
2025-12-14 22:27:05 +0200

    Remove The Oldeset Code

--- src/main.py
+++ src/main.py
@@ -1,21 +1,3 @@
-# def run_prediction(mutatetype, slctype, crovertype):
-#     from ga.population import Population 
-#     from data.loadData import LoadData
-#     from ga.geneticAlgorithm import GeneticAlgorithm
-#     from PIL import ImageTk, Image
-#     loadDataObj = LoadData()
-#     teams = loadDataObj.load_teams()
-#     venues = loadDataObj.load_venues()
-#     timeslots = loadDataObj.load_timeslots()
-#     times = loadDataObj.load_times_from_csv()
-#     gaObj = GeneticAlgorithm()
-#     best_schedule = gaObj.runAlgorithm(teams, venues, timeslots,times, crovertype, slctype, mutatetype)
-#     if best_schedule.fitness_score < 0:
-#         print("No feasible solution found.")
-#         print(best_schedule.fitness_score)
-#     else:
-#         best_schedule.display()
-
 from Create_Plots import PlotManager
 
 def run_prediction(mutatetype, slctype, crovertype):
